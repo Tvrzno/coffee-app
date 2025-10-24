52859d70085caaa1247d021e2f9b6693d76deef4
Karolina Mukarovska <k.mukarovska01@gmail.com>
2025-10-24 12:13:59 +0200

    navBar, HomePage

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router';
+import homeIcon from './images/home-icon.png';
+import addIcon from './images/add-icon.png';
+import recordsIcon from './images/records-icon.png';
+import './NavBar.scss';
 
 export const NavBar = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/add">Add</Link>
-      <Link to="/records">Records</Link>
-    </div>
+    <nav className="navBar">
+      <div className="navBar__links">
+        <Link className="navBar__link" to="/" aria-label="Domů">
+          <img className="navBar__icon" src={homeIcon} alt="Domů" aria-hidden />
+        </Link>
+        <Link className="navBar__link" to="/add" aria-label="Přidat záznam">
+          <img className="navBar__icon" src={addIcon} alt="Přidat záznam" aria-hidden />
+        </Link>
+        <Link className="navBar__link" to="/records" aria-label="Zobrazit záznamy">
+          <img className="navBar__icon" src={recordsIcon} alt="Zobrazit záznamy" aria-hidden />
+        </Link>
+      </div>
+    </nav>
   );
 };
