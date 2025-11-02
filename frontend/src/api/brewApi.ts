1a7c66dc13c2ee787f6360000f77419de6226845
Karolina Mukarovska <k.mukarovska01@gmail.com>
2025-11-02 09:56:22 +0100

    fix last brews fetch

--- frontend/src/api/brewApi.ts
+++ frontend/src/api/brewApi.ts
@@ -1,7 +1,7 @@
 import type { Brew } from '../types/brews';
 
 export const fetchLastBrews = async (): Promise<Brew[]> => {
-  const response = await fetch('api/last_5_joined_brews');
+  const response = await fetch('api/last_brews');
 
   if (!response.ok) {
     throw new Error('Nepodařilo se načíst přípravy');
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -23,8 +23,8 @@ export const HomePage = () => {
       <section className="homePage__hero">
         <h1 className="homePage__header">BrewDiary</h1>
       </section>
-      <section className="homePage__recordsWrap">
-        <h2>Poslední záznamy</h2>
+      <section className="lastRecords">
+        <h2 className="lastRecords__title">Poslední záznamy</h2>
         {lastBrews?.map(brew => (
           <Link className="record" to={`/`}>
             <h2 className="record__name">{brew.note}</h2>
